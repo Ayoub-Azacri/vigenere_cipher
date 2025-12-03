e38f766494728c0b7d93cf57f74000a55f8a90c6
Ayoub-Azacri <ayoub.azacri@gmail.com>
2025-12-03 14:17:53 +0100

    bug fix : correction of the out of range bug related to ascii table

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 
-
+# chifferement
+text = "lapin"
+key = 3
 def cesar_cipher(text, key):
     crypted_text = ""
     for char in text:
         # find position of char ascii and add a décalage
-        crypted_char = chr((ord(char) + key))
+        crypted_char = chr((ord(char) + key) % 1_114_112)
         crypted_text += crypted_char
     return crypted_text
 
-crypted_text = cesar_cipher(text="lapin", key=3)
-print(crypted_text)
+print(f"Given text: {text}")
+crypted_text = cesar_cipher(text,key)
+print(f"Crypted text: {crypted_text}")
