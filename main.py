0aa0bd63118980d12eb36602808e041dae7d372d
Ayoub-Azacri <ayoub.azacri@gmail.com>
2025-12-03 14:12:44 +0100

    bug fix : correction of the out of range bug related to ascii table

--- main.py
+++ main.py
@@ -5,24 +5,12 @@ key = 3
 def cesar_cipher(text, key):
     crypted_text = ""
     for char in text:
-        # find position of char ascii and add a décalage
-        crypted_char = chr((ord(char) + key))
+        #? find position of char ascii and add a décalage
+        crypted_char = chr((ord(char) + key) % 1_114_112)
         crypted_text += crypted_char
     return crypted_text
 
 print(f"Given text: {text}")
 crypted_text = cesar_cipher(text,key)
 print(f"Crypted text: {crypted_text}")
-
-#déchifferement
-def dechiffre_cesar(text, key):
-    decrypted_text = ""
-    for char in text:
-        # find position of char ascii and remove the décalage
-        decrypted_char = chr((ord(char) - key))
-        decrypted_text += decrypted_char
-    return decrypted_text
-
-decrypted_text = dechiffre_cesar(crypted_text, key)
-print(f"Deccrypted text: {decrypted_text}")
 
